f0fb6fa9e8c2802997dec9ae89e328594940a74a
Paul Engel <paul.engel@holder.nl>
2013-03-03 12:17:06 +0100

    Updated core_ext.rb

--- lib/moc_ruby/core_ext.rb
+++ lib/moc_ruby/core_ext.rb
@@ -1,20 +1,9 @@
-module Singleton
-  def self.extend_object(*args, &block)
-    puts "Called Singleton.extend_object from #{caller[0].match(/^[^:]+/)}"
-  end
-end
+# MacRuby undefines the following methods
 
-class StandardError
-  def chain(*args, &block)
-    puts "Called #{self.class}.chain from #{caller[0].match(/^[^:]+/)}"
-  end
-  def chain=(*args, &block)
-    puts "Called #{self.class}.chain= from #{caller[0].match(/^[^:]+/)}"
-  end
+module Singleton
+  def self.extend_object(obj); end
 end
 
 class Pathname
-  def =~(*args, &block)
-    puts "Called #{self.class}.=~ from #{caller[0].match(/^[^:]+/)}"
-  end
+  def =~(regexp); end
 end
